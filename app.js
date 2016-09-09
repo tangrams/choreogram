f288688bf57f841c35f48d5718453dae67c60b81
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-09-09 13:46:58 -0400

    clean log

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-var fs = require( 'fs' );
-var app = require('express')();
-var https = require('https');
+var fs = require('fs');
 var path = require('path');
+var https = require('https');
+var http = require('http');
 
-var SERVER_KEY = 'key.pem';
-var SERVER_CRS = 'cert.pem';
-var HTTP_PORT = 8080;
+var express = require('express');
+var app = express();
 
-var server = https.createServer({
-    key: fs.readFileSync(SERVER_KEY),
-    cert: fs.readFileSync(SERVER_CRS)
-},app);
-server.listen(HTTP_PORT);
+var options = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
+var HTTPS_PORT = 8080;
 
-var io = require('socket.io').listen(server);
+var server = https.createServer(options, app);
+// var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
     socket.broadcast.emit('user connected');
@@ -41,4 +42,8 @@ app.get('/src/promise-7.0.4.min.js',function(req,res){
 
 app.get('/src/leaflet-hash.js',function(req,res){
   res.sendFile(path.join(__dirname+'/src/leaflet-hash.js'));
+});
+
+server.listen(HTTPS_PORT, function() {
+  console.log('server up and running at %s port', HTTPS_PORT);
 });
